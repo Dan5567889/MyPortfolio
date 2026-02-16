15a4df231548622b18e9d873b3e04945eeaac48c
Dan5567889 <92761532+Dan5567889@users.noreply.github.com>
2026-02-16 15:56:43 +0000

    ourbloom page added

--- app/Projects/page.tsx
+++ app/Projects/page.tsx
@@ -8,7 +8,7 @@ export default function Projects() {
         <div className="flex flex-col text-center items-center gap-6 mt-10">
             <div className="bg-[#2C3333] text-[#F3F4F4] p-5 rounded-lg w-full max-w-md">
             <h2 className="text-2xl font-semibold mb-2">Project 1: Our Blooms</h2>
-            <Button text="Learn More!" href="#" className="bg-yellow-400 text-black px-4 py-2 rounded inline-block"/>
+            <Button text="Learn More!" href="/project1" className="bg-yellow-400 text-black px-4 py-2 rounded inline-block"/>
             </div>
 
             <div className="bg-[#2C3333] text-[#F3F4F4] p-5 rounded-lg w-full max-w-md">
