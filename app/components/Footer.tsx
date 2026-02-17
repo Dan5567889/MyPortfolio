8286fb7d2b119458be149633ebd3774226ba1a13
Dan5567889 <dantehernandz587@gmail.com>
2026-02-17 08:53:45 -0500

    contact me

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+import Button from "./Button";
+
 export default function Footer() {
     return (
       <div>
         <footer className="bg-[#2C3333] text-[#F3F4F4] p-5 text-center">
-          <h1>GET IN TOUCH!</h1>
-          <h2>Email me at: dantehernandez011508@gmail.com</h2>
+          <Button text="CONTACT ME" href="/contactMe"/>
         </footer>
       </div>
     );
---
+++ app/contactMe/page.tsx
@@ -0,0 +1,39 @@
+export default function ContactMe() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-[#2E4F4F] text-[#F3F4F4] p-8">
+            <h1 className="text-4xl font-bold mb-6">Contact Me</h1>
+            <form className="flex flex-col gap-4 w-full max-w-md bg-[#0E8388] p-6 rounded-lg shadow-lg">
+                <label className="flex flex-col">
+                    <span className="font-semibold mb-2">Name:</span>
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        className="p-2 rounded border border-gray-300"
+                    />
+                </label>
+                <label className="flex flex-col">
+                    <span className="font-semibold mb-2">Email:</span>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        className="p-2 rounded border border-gray-300 "
+                    />
+                </label>
+                <label className="flex flex-col">
+                    <span className="font-semibold mb-2">Message:</span>
+                    <textarea
+                        placeholder="Enter your message"
+                        className="p-2 rounded border border-gray-300 "
+                        rows={5}
+                    ></textarea>
+                </label>
+                <button
+                    type="submit"
+                    className="bg-cyan-600 text-white font-semibold py-2 px-4 rounded hover:bg-cyan-700 "
+                >
+                    Submit
+                </button>
+            </form>
+        </div>
+    );
+}
