aa70986e36af4d5d70bc056ad6009882b5c7d784
Dan5567889 <dantehernandz587@gmail.com>
2026-02-17 09:12:39 -0500

    underline

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,7 +7,9 @@ export default function Footer() {
         <footer className="bg-[#2C3333] text-[#F3F4F4] p-5 text-center">
         <p className="text-lg mt-2 text-stone-400">If you have any questions feel free to contact me!</p>
           <br/>
-          <Button text="CONTACT ME!" href="/contactMe" className="font-bold-text"/>
+          
+          <Button text="CONTACT ME!" href="/contactMe" className="font-bold-text underline underline-offset-1"/>
+          
         </footer>
         
 
