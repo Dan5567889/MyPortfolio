aa5eeb117016fd0590f8d7a9d0867329f22aeed6
Dan5567889 <dantehernandz587@gmail.com>
2026-02-17 09:08:13 -0500

    footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,8 +5,12 @@ export default function Footer() {
     return (
       <div>
         <footer className="bg-[#2C3333] text-[#F3F4F4] p-5 text-center">
-          <Button text="CONTACT ME" href="/contactMe"/>
+        <p className="text-lg mt-2 text-stone-400">If you have any questions feel free to contact me!</p>
+          <br/>
+          <Button text="CONTACT ME!" href="/contactMe" className="font-bold-text"/>
         </footer>
+        
+
       </div>
     );
   }
